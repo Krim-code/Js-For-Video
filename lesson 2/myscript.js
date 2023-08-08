ccafa7cc690a4550c3b217394417e6d6db949db0
krim-code <suhetsky.gricha555@gmail.com>
2023-08-08 22:58:41 +0300

    second lessons

---
+++ lesson 2/myscript.js
@@ -0,0 +1,39 @@
+// let a = 10
+// let b = 10
+
+// alert(a > b)
+// alert(a < b)
+// alert(a == b) // обычное сравнение
+// alert(a === b) // сравнение по типу
+// alert(a <= b)
+// alert(a >= b)
+
+
+// let num = parseInt(prompt("Введите число"))
+
+// if (num > 0){
+//     alert("больше")
+// }
+// else if(num === 0) {
+//     alert("равно")
+// }
+// else{
+//     alert("меньше")
+// }
+
+// let num = parseInt(prompt("Введите возраст"))
+
+// if (num > 0 && num <= 120){
+//     alert("Welcome")
+// }
+// else{
+//     alert("error")
+// }
+
+// let num = parseInt(prompt("Введите число"))
+
+// if (num > 0 || num < -25){
+//     alert("Welcome")
+// }
+// else{
+//      alert("error") }
