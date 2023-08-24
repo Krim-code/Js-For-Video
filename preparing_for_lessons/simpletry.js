9e24aad01f869c9c4eb880480913e541aaef0c8e
AntoxaElros <antoxa11091984@ya.ru>
2023-08-24 16:12:18 +0300

    modified:   preparing_for_lessons/simpletry.js

--- preparing_for_lessons/simpletry.js
+++ preparing_for_lessons/simpletry.js
@@ -1,17 +1,28 @@
+// t = "adbrt"
+
+str = "abop gropg k4og krqk paoqkweo kr 3okoh kohkaaaa aaa aa aaa"
+// count= 0
+// for (char of str){
+//     if(t.includes(char)){
+//         count++
+//     }
+// }
+// console.log(count)
 
 
-const sumRec = function(num){
-    if(num === 1){
-        return 1
+
+function trancate(str, maxLength){
+    if (str.length > maxLength){
+        str = str.slice(0,maxLength)+"..."
     }
-    else{
-        console.log(`${num} + sumRec(${num-1})`)
-        return num + sumRec(num-1)
-    }
+    return str
 }
 
-console.log(sumRec(10))
+console.log(trancate(str,6))
 
-for (let i = 0; i < 10; i++){
-    console.table(i)
-}
+console.log(str.charAt(6))
+
+console.log(str.split(" ").length)
+
+let myDate = new Date(2012,11,25,18,30,20,10)
+console.log(myDate.get)
